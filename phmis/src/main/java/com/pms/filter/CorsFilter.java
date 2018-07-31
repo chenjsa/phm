9c028164e95ffdd64b92babe9701259142bb3a47
toyzhou <2864333077@qq.com>
2018-07-31 20:38:45 +0800

    test

--- phmis/src/main/java/com/pms/filter/CorsFilter.java
+++ phmis/src/main/java/com/pms/filter/CorsFilter.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Component;  
    
-@Component  
+@Component
 public class CorsFilter implements Filter {  
   
     final static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(CorsFilter.class);  
